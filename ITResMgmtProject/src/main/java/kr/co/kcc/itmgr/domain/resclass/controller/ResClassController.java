dc85f13783ed7cba40b7d3fc1f7606f543899be6
hannahjjjj <wh467925@gmail.com>
2023-08-30 18:00:02 +0900

    [한나]feat:resClass초안

--- ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/domain/resclass/controller/ResClassController.java
+++ ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/domain/resclass/controller/ResClassController.java
@@ -1,5 +1,26 @@
 package kr.co.kcc.itmgr.domain.resclass.controller;
 
-public class ResClassController {
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.co.kcc.itmgr.domain.resclass.model.ResClass;
+import kr.co.kcc.itmgr.domain.resclass.service.IResClassService;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ResClassController {
+	
+	private final IResClassService IResClassService;
+	
+	@RequestMapping(value="/resclass", method=RequestMethod.GET)
+	public String selectAllResClass(Model model) {
+		List<ResClass> resClassList = IResClassService.selectAllResClass();
+		model.addAttribute("resClassList", resClassList);
+		return "resclass/resclass";
+	}
 }
--- ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/domain/resclass/dao/IResClassRepository.java
+++ ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/domain/resclass/dao/IResClassRepository.java
@@ -1,5 +1,15 @@
 package kr.co.kcc.itmgr.domain.resclass.dao;
 
-public interface IResClassRepository {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import kr.co.kcc.itmgr.domain.resclass.model.ResClass;
+
+@Repository
+@Mapper
+public interface IResClassRepository {
+	List<ResClass> selectAllResClass();	
+	
 }
