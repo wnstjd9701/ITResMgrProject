c3604cf62e934eced0b86b7a1cdfab36ab6cf5c1
minseonseon <ms9732@naver.com>
2023-08-30 10:58:10 +0900

    first

--- ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/domain/employee/model/Employee.java
+++ ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/domain/employee/model/Employee.java
@@ -1,5 +1,13 @@
 package kr.co.kcc.itmgr.domain.employee.model;
 
-public class Employee {
+import java.sql.Timestamp;
 
+public class Employee {
+	private String employeeId;
+	private String employeePwd;
+	private String employeeName;
+	private String employeeTypeCode;
+	private String employeeStatusCode;
+	private Timestamp createDate;
+	private String createrId;
 }
