2af73a334b84f19bf611781e3fb5f3893ab8163f
wnstjd9701 <wnstjd9701@naver.com>
2023-09-05 00:05:29 +0900

    [준] feat: 폴더 구조 변경

--- ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/domain/home/controller/MonitoringController.java
+++ ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/domain/home/controller/MonitoringController.java
@@ -8,12 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.kcc.itmgr.domain.home.model.Monitoring;
 import kr.co.kcc.itmgr.domain.home.service.IMoniteringService;
-import kr.co.kcc.itmgr.domain.home.service.MonitoringService;
 import kr.co.kcc.itmgr.domain.resclass.model.ResClass;
 import lombok.RequiredArgsConstructor;
 
