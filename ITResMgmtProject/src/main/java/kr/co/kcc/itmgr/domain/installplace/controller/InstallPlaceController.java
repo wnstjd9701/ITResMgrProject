a7bca14f144981513efd66be5f283426d319a854
wnstjd9701 <wnstjd9701@naver.com>
2023-10-05 18:01:05 +0900

    [준성] feat: 전체 수정

--- ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/domain/installplace/controller/InstallPlaceController.java
+++ ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/domain/installplace/controller/InstallPlaceController.java
@@ -20,17 +20,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.kcc.itmgr.domain.installplace.model.InstallPlace;
 import kr.co.kcc.itmgr.domain.installplace.model.InstallRes;
+import kr.co.kcc.itmgr.domain.installplace.model.PlaceApiResponse;
 import kr.co.kcc.itmgr.domain.installplace.service.IInstallPlaceService;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class InstallPlaceController {
-	
+
 	static final Logger logger = LoggerFactory.getLogger(InstallPlaceController.class);
-	
+
 	private final IInstallPlaceService installPlaceService;
-	
+
 	/*
 	 * @Author: [윤준성]
 	 * @API No.3-1. 설치 장소 페이지
@@ -41,10 +42,10 @@ public class InstallPlaceController {
 		List<InstallPlace> installPlace = installPlaceService.selectAllPlace();
 		logger.info("installPlace: " + installPlace);
 		model.addAttribute("installPlace", installPlace);
-		
+
 		return "place/installplace";
 	}
-	
+
 	/*
 	 * @Author: [윤준성]
 	 * @API No.3-2. 설치 장소 검색 [비동기]
@@ -57,7 +58,7 @@ public class InstallPlaceController {
 		logger.info("InstallPlaceList: " + installPlace);
 		return installPlace;
 	}
-	
+
 	/*
 	 * @Author: [윤준성]
 	 * @API No.3-3. 자원 정보 및 상세 주소 조회 [비동기]
@@ -68,38 +69,40 @@ public class InstallPlaceController {
 	public Map<String, Object> selectResInformationByInstallPlaceName(String placeName){
 		List<InstallRes> resInfo = installPlaceService.selectResInformationByInstallPlaceName(placeName);
 		InstallPlace installPlace = installPlaceService.selectInstallPlaceDetail(placeName);
-		
+
 		Map<String, Object> placeMap = new HashMap<String, Object>();
 		placeMap.put("resInfo", resInfo);
 		placeMap.put("installPlace", installPlace);
 		return placeMap;
 	}
-	
-	/* @Author: [윤준성]
-	 * @API No.3-6. 설치 장소명 중복 체크
-	 * @Info: 장소명이 이미 등록되어 있는지 확인 
-	 */
-	@GetMapping("/check/name")
-	@ResponseBody
-	public int checkPlaceName(@RequestParam("placeName") String placeName) {
-		int result = installPlaceService.checkPlaceNameBySn(placeName);
-		logger.info("result: " + result);
-		return result;
-	}
+
 	/*
 	 * @Author: [윤준성]
 	 * @API No.3-4. 주소 등록 [비동기]
 	 * @Info: 상세 주소 등록 
 	 */
 	@PostMapping("/detailaddress")
-	@ResponseBody
-	public List<InstallPlace> installPlaceSave(@RequestBody InstallPlace installPlace) {
-		int result = installPlaceService.insertInstallPlace(installPlace);
-		logger.info("Test: " + installPlace);
+	public ResponseEntity<PlaceApiResponse> installPlaceSave(@RequestBody InstallPlace installPlace) {
+		PlaceApiResponse response = new PlaceApiResponse();
+		
+		String placeName = installPlace.getInstallPlaceName();
+		int nameCheck = installPlaceService.checkPlaceNameBySn(placeName);
+		if(nameCheck >= 1) {
+			response.setStatus("error");
+            response.setCode(400); 
+            response.setMessage("이미 존재하는 장소 이름입니다.");
+			return ResponseEntity.ok().body(response);
+		}
+		int res = installPlaceService.insertInstallPlace(installPlace);
 		List<InstallPlace> newInstallPlace = installPlaceService.selectAllPlace();
-		return newInstallPlace;
+		
+		response.setStatus("success");
+        response.setCode(200); 
+        response.setMessage(res + "개의 장소가 성공적으로 저장되었습니다.");
+        response.setData(newInstallPlace);
+		return ResponseEntity.ok().body(response);
 	}
-	
+
 	/*
 	 * @Author: [윤준성]
 	 * @API No.3-5. 주소 수정 [비동기]
@@ -107,12 +110,27 @@ public class InstallPlaceController {
 	 */
 	@PostMapping("/detailaddress/{placesn}")
 	@ResponseBody
-	public List<InstallPlace> updateInstallPlace(@PathVariable("placesn") String placesn, @RequestBody InstallPlace installPlace) {
-		int result = installPlaceService.updateInstallPlace(installPlace);
+	public ResponseEntity<PlaceApiResponse> updateInstallPlace(@PathVariable("placesn") String placesn, @RequestBody InstallPlace installPlace) {
+		PlaceApiResponse response = new PlaceApiResponse();
+		
+		String placeName = installPlace.getInstallPlaceName();
+		int nameCheck = installPlaceService.checkPlaceNameBySn(placeName);
+		if(nameCheck >= 1) {
+			response.setStatus("error");
+            response.setCode(400); 
+            response.setMessage("이미 존재하는 장소 이름입니다.");
+			return ResponseEntity.ok().body(response);
+		}
+		int res = installPlaceService.updateInstallPlace(installPlace);
 		List<InstallPlace> newInstallPlace = installPlaceService.selectAllPlace();
-        return newInstallPlace;
+		
+		response.setStatus("success");
+        response.setCode(200); 
+        response.setMessage("장소가 성공적으로 수정되었습니다.");
+        response.setData(newInstallPlace);
+		return ResponseEntity.ok().body(response);
 	}
-	
+
 	/*
 	 *  @Author: [윤준성]
 	 *  @API No.3-6. 주소 삭제 [비동기]
@@ -120,14 +138,14 @@ public class InstallPlaceController {
 	 */
 	@PostMapping("/delete/place")
 	public ResponseEntity<Object> deleteInstallPlace(@RequestParam("placesn") int placesn) {
-	    logger.info("placesn: " + placesn);
-	    boolean result = installPlaceService.deleteInstallPlace(placesn);
+		logger.info("placesn: " + placesn);
+		boolean result = installPlaceService.deleteInstallPlace(placesn);
 
-	    if (result) {
-	        List<InstallPlace> newInstallPlace = installPlaceService.selectAllPlace();
-	        return ResponseEntity.ok(newInstallPlace); // 성공 시 200 OK와 데이터 반환
-	    } else {
-	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("fail"); // 실패 시 500 Internal Server Error와 실패 메시지 반환
-	    }
+		if (result) {
+			List<InstallPlace> newInstallPlace = installPlaceService.selectAllPlace();
+			return ResponseEntity.ok(newInstallPlace); // 성공 시 200 OK와 데이터 반환
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("fail"); // 실패 시 500 Internal Server Error와 실패 메시지 반환
+		}
 	}
 }
---
+++ ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/global/exception/EnumModel.java
@@ -0,0 +1,6 @@
+package kr.co.kcc.itmgr.global.exception;
+
+public interface EnumModel {
+	  String getKey();
+	  String getValue();
+}
