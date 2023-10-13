f9bbb76fc3f807d31fbc3164b231868638cb480a
minseonseon <ms9732@naver.com>
2023-10-13 14:23:18 +0900

    [민선] feat : 로그인 기능 완성

--- ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/domain/user/dao/User.java
+++
@@ -1,8 +0,0 @@
-package kr.co.kcc.itmgr.domain.user.dao;
-
-import lombok.Data;
-
-@Data
-public class User {
-
-}
--- ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/domain/user/service/UserService.java
+++ ITResMgmtProject/src/main/java/kr/co/kcc/itmgr/domain/user/service/UserService.java
@@ -2,7 +2,19 @@ package kr.co.kcc.itmgr.domain.user.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserService implements IUserService{
+import kr.co.kcc.itmgr.domain.user.dao.IUserRepository;
+import kr.co.kcc.itmgr.domain.user.model.User;
+import lombok.RequiredArgsConstructor;
 
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements IUserService{
+	private final IUserRepository userRepository;
+
+	@Override
+	public User selectUser(String employeeId) {
+		return userRepository.selectUser(employeeId);
+		
+	}
 }
